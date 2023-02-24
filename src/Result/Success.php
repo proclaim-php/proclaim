2d75fb5a17657015a39f237cbcae2ecfd7bf7a1e
Marijn van Wezel <marijnvanwezel@gmail.com>
2023-02-24 22:38:43 +0100

    Create classes that will contain result values

---
+++ src/Result/Success.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of Proclaim.
+ *
+ * Copyright (C) Marijn van Wezel
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Proclaim\Result;
+
+final class Success
+{
+
+}
